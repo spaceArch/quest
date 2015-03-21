be8d6c48eaeb96707be9a7b2152daafd0c8c2a36
Sergey STARKOV <sergeystarkov92@gmail.com>
2015-03-21 14:30:13 +0300

    fix form quest create new quest

--- client/views/addQuest/add_quest.js
+++ client/views/addQuest/add_quest.js
@@ -1,9 +1,61 @@
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+var images = [];
+
 Template.addQuest.created = function() {
   Uploader.uploadUrl = Meteor.absoluteUrl("upload");
+  Session.set("questId", makeid());
+
+  console.log('make id', Session.get("questId"));
 };
 
 Template.addQuest.helpers({
-  arrivals: function () {
-    return ArrivalRepo.findNearest();
+  uploadingImages: function() {
+    return {
+      finished: function(index, fileInfo, context) {
+        images.push(fileInfo);
+      }
+    }
+  }
+});
+
+Template.addQuest.events({
+  'submit form': function (e) {
+    e.preventDefault();
+
+    console.log('submit form!');    
+    console.log('uploadedImages', images);
+
+    var questTitle = $(e.target).find('[name=titleQuest]').val(),
+        questSummary = $(e.target).find('[name=summaryQuest]').val();
+
+    var quest = {
+      quest_id: Session.get('questId'),
+      quest_title: questTitle,
+      quest_summary: questSummary,
+      quest_images: images,
+      autor_id: Meteor.userId(),
+      created_at: new Date().getTime()
+    }
+    
+    console.log('quest ' , quest);
+
+    Meteor.call('addQuest', quest, function (error, result) {
+      if (error) {
+        return alert(error.reason);
+      }
+      else {
+        console.log('Quest added!');
+        // goto quest page
+      }
+    });
+
   }
 });
--- collections/quests.js
+++ collections/quests.js
@@ -1,10 +1,39 @@
 Quests = new Meteor.Collection('quests');
 
+// {
+//   quest_id: '12345',
+//   quest_title: 'Test title',
+//   quest_summary: 'Test summary',
+//   quest_images: ['test1.png', 'test2.pgng'],
+//   autor_id: '12345',
+//   created_at: 'datetime'
+// }
+
 Quests.allow({
   update: function(userId, doc) {
     return !! userId;
   },
   remove: function(userId, doc) {
     return !! userId;
+  }
+});
+
+
+Meteor.methods({
+  addQuest: function(quest) {
+    var user = Meteor.user(),
+        questWithSameId = QuestRepo.findOne({quest_id: quest.quest_id});
+
+    if (!user)
+      throw new Meteor.Error(401, "You need to login");
+    if (questWithSameId)
+      throw new Meteor.Error(409, 'Duplicate quests');
+
+    var quest = _.extend(
+      _.pick(quest, 'quest_id', 'quest_title', 'quest_summary', 'quest_images','autor_id'), {
+        created_at: new Date().getTime()
+    });
+
+    QuestRepo.insert(quest);
   }
 });
