1ca02ab2065324a06063e5129a3253367cbc956a
Sergey STARKOV <sergeystarkov92@gmail.com>
2015-03-21 20:57:38 +0300

    fix styles fix lauout

--- client/views/questPage/questPage.js
+++ client/views/questPage/questPage.js
@@ -6,6 +6,11 @@ Template.questPage.created = function() {
   });
 };
 
+Template.questPage.rendered = function() {
+  $('.toggle-navbar').removeClass("hide");
+  $('.navbar').removeClass("expanded");
+};
+
 Template.questPage.helpers({
   pageId: function () {
     return Session.get('questId');
