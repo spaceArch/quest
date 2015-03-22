59c9d864832e4f15fb6d4fb2c1caf2ccf52c5838
Sergey STARKOV <sergeystarkov92@gmail.com>
2015-03-22 12:59:55 +0300

    add masonry

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,6 +1,17 @@
 Template.home.rendered = function() {
   UIRepo.changeContentView(false);
   $('body>div[style]').remove();
+
+  $('#container').masonry({
+      itemSelector: '.quest-item',
+      singleMode: true,
+      isResizable: true,
+      isAnimated: true,
+      animationOptions: { 
+        queue: false, 
+        duration: 500 
+      }
+    }); 
 };
 
 Template.home.helpers({
