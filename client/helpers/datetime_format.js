ee90aa1ce75dab1f8a178ee8256374cbd651a96c
Sergey STARKOV <sergeystarkov92@gmail.com>
2015-03-22 16:08:11 +0300

    fix ui

--- client/helpers/datetime_format.js
+++ client/helpers/datetime_format.js
@@ -1,3 +1,3 @@
 Handlebars.registerHelper("getDateTime", function(timestamp) {
-  return moment(timestamp).format('LLL');
+  return moment(timestamp).format('LL');
 });
--- client/helpers/link_to_thumb.js
+++ client/helpers/link_to_thumb.js
@@ -2,3 +2,7 @@ Handlebars.registerHelper("linkToThumb", function(images) {
   var imagePath = images[0].previewPath
   return '/' + imagePath;
 });
+
+Handlebars.registerHelper("countImages", function(images) {
+  return images.length;
+});
