d667e139a3a2597aebd44586a900f7f8cbf94e64
Sergey STARKOV <sergeystarkov92@gmail.com>
2015-03-22 11:44:03 +0300

    fix publications

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,11 @@
 Meteor.publish('quests', function() {
   return QuestRepo.findAll();
 });
+
+Meteor.publish('questsByUser', function(userId) {
+  return Quests.find({autor_id: userId});
+});
+
+Meteor.publish('questItem', function(questId) {
+  return Quests.find({quest_id: questId});
+});
