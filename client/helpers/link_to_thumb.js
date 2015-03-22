948fb2492123c5b8bce7ab447fe6575760377ebc
Sergey STARKOV <sergeystarkov92@gmail.com>
2015-03-22 05:34:55 +0300

    fix path

--- client/helpers/link_to_thumb.js
+++ client/helpers/link_to_thumb.js
@@ -1,4 +1,4 @@
 Handlebars.registerHelper("linkToThumb", function(images) {
   var imagePath = images[0].previewPath
-  return 'http://188.226.222.86:8080/' + imagePath;
+  return '/' + imagePath;
 });
