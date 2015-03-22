0c9248bf5e217a61bfecdb5ef274984f240c7757
Sergey STARKOV <sergeystarkov92@gmail.com>
2015-03-22 06:40:16 +0300

    fix

--- client/views/questPage/questPage.js
+++ client/views/questPage/questPage.js
@@ -3,6 +3,9 @@ Template.questPage.created = function() {
 };
 
 Template.questPage.rendered = function() {
-  $('.toggle-navbar').removeClass("hide");
-  $('.navbar').removeClass("expanded");
+  $('.toggle-navbar').addClass("hide");
+  $('.map-filters').addClass("hide");
+  $('.navbar').addClass("expanded");
+  $('body').removeClass("map-view");
+  initMap.call(this);
 };
